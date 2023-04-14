15865a2278680fe8bde39511479214fe63a9f936
Kevin Bravo <kbravo1024@gmail.com>
2023-04-13 18:39:10 -0700

    complete task lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const Formtask = document.getElementById("create-task-form");
+  const descriptionTask = document.getElementById("new-task-description");
+  const createNewTask = document.getElementById("Create New Task");
+  const tasksToDo = document.getElementById("tasks")
+  
+  function displayList(){
+    const span = document.createElement("span");
+    const li = document.createElement("li");
+    const dltButton = document.createElement("button");
+ 
+    span.textContent = `${descriptionTask.value}`;
+    dltButton.innerHTML = " remove "
+
+    span.appendChild(dltButton);
+    li.appendChild(span);
+    tasksToDo.appendChild(li)
+  }
+
+  Formtask.addEventListener("submit", (e) => {
+    e.preventDefault() 
+    displayList()
+    e.target.reset();
+  })
+
+  tasksToDo.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(confirm("Are you sure?"))
+      e.target.parentNode.parentNode.remove()
+  })
+}
+)
